2f79caa06326ccba7d260725e7f289d21ef46ca8
MdShihab <shihabshahriar@iut-dhaka.edu>
2020-08-09 21:32:47 +0600

    updated schema and routes for projects

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 const DB = require('../config/connectDB')
+const { v4: uuidv4 } = require('uuid');
 
 
 //Create a passport middleware to handle user registration
@@ -20,6 +21,7 @@ passport.use('register', new localStrategy({
       const hash = await bcrypt.hash(password, 10);
       password = hash;
         const sqlData = {
+            id: uuidv4(),
             name: name,
             email: email,
             password: password,
--- models/User.js
+++ models/User.js
@@ -4,13 +4,13 @@ const DB = require('../config/connectDB')
 const CreateUserSchema = async () => {
     try {
         sqlQuery = `CREATE TABLE IF NOT EXISTS user(
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(100),
-            email VARCHAR(100) UNIQUE,
+            id VARCHAR(255) PRIMARY KEY,
+            name VARCHAR(255),
+            email VARCHAR(255) UNIQUE,
             emailVerify BOOLEAN NOT NULL DEFAULT TRUE,
             password VARCHAR(255),
-            organization VARCHAR(100),
-            position VARCHAR(100),
+            organization VARCHAR(255),
+            position VARCHAR(255),
             createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )`
         await DB.pool.query(sqlQuery)
