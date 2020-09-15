e34cc7e183861606c95c0bdb8a10cb15a79c2761
refaat31 <refaatalamgir@iut-dhaka.edu>
2020-09-15 15:41:51 +0600

    get all projects of a specific User
    for displaying in user dashboard in front end

--- routes/project.js
+++ routes/project.js
@@ -4,12 +4,12 @@ const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
-
 const Project = require('../models/Project');
 const Comment = require('../models/Comment');
 //const { post } = require('./users');
 
 const validateCommentInput = require('../validation/comment');
+
 //  @route GET api/project
 //  @desc get all projects
 //  @access public
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const project = await Project.getProjectById(req.params.id);
-    
+
     if (!project[0]) {
       return res.status(400).json({ msg: 'no project found' });
     }
@@ -52,18 +52,18 @@ router.post(
     try {
       let newProject = {
         id: uuidv4(),
-        created_by_id:req.user.id,
-        project_name:req.body.project_name,
-        project_type:req.body.project_type,
-        project_description:req.body.project_description,
-        start_date:new Date(req.body.start_date),
-        end_date:new Date(req.body.end_date),
-        goal:req.body.goal,
+        created_by_id: req.user.id,
+        project_name: req.body.project_name,
+        project_type: req.body.project_type,
+        project_description: req.body.project_description,
+        start_date: new Date(req.body.start_date),
+        end_date: new Date(req.body.end_date),
+        goal: req.body.goal,
         //project_status_id:uuidv4(),
         //project_video_url:req.body.project_video_url
       };
 
-      await Project.createNewProject(newProject,next);
+      await Project.createNewProject(newProject, next);
       res.json({ msg: 'created a new project' });
     } catch (error) {
       console.log(error.message);
@@ -151,6 +151,26 @@ router.delete(
     try {
       await Comment.DeleteCommentById(req.params.comment_id, req.params.id);
       res.json({ msg: 'comment deleted successfully' });
+    } catch (error) {
+      res.status(500).send('server error');
+    }
+  }
+);
+
+//  @route GET api/project/myprojects
+//  @desc get user's projects
+//  @access private
+
+router.get(
+  '/personal/all',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const myprojects = await Project.getProjectsByUserId(req.user.id);
+      if (!myprojects[0]) {
+        return res.status(400).json({ msg: 'you do not have any projects' });
+      }
+      res.json(myprojects[0]);
     } catch (error) {
       res.status(500).send('server error');
     }
